d6e4c9603e23e1e864f2ac8819808fd8ce1c3b44
vogmur <vogmur86@gmail.com>
2026-08-28 22:00:34 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 // Ждём, пока сайт загрузится
 document.addEventListener(
-   "DOMContentLoaded", () =>{
-    // Находим все кнопки и аккордеоны (details/summary) на странице
-    const elements = document.querySelectorAll('button, .btn, summary');
-    elements.forEach(el => {
-        el.addEventListener('click', () =>
-        {
-            // Запускаем конфетти при клике
-            confetti({
-                particleCount: 100,
-                spread: 70,
-                origin: { y:0.6 }
-            })
+   "DOMContentLoaded", () =>
+   {
+    const surpriseBtn = document.getElementById('surpriseBtn');
+   if (surpriseBtn) {
+    surpriseBtn.addEventListener('click', () =>
+    {
+        confetti({
+            particleCount: 100,
+            spread: 70,
+            origin: { y: 0.6 }
         })
     })
-        
+   }
+   }
